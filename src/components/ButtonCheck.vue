84164f8dd4bc246b9e5dcfabc7eddf044c6d9cab
damouu <mouadsehbaoui@gmail.com>
2024-08-30 14:56:08 +0200

    ButtonCheck.vue changes the Icon name in the top left corner

--- src/components/ButtonCheck.vue
+++ src/components/ButtonCheck.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link v-slot="{ navigate }" custom to="/">
     <button class="btn rounded ms-2 me-2" role="link" @click="navigate">
-      <i class="bi bi-chat-quote h4">VueApp</i>
+      <i class="bi bi-chat-quote h4">LibraryApp</i>
     </button>
   </router-link>
 </template>
