1ccc264cc71e71bd556df2ec5861b1605871d757
damouu <mouadsehbaoui@gmail.com>
2024-08-30 18:45:53 +0200

    changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import DataView from "@/components/DataView.vue";
-import NewsView from "@/views/NewsView.vue";
-import StudentView from "@/views/UserView.vue";
+import DataView from "@/views/BookView.vue";
+import NewsView from "@/views/BookListView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,26 +12,15 @@ const router = createRouter({
             component: HomeView
         },
         {
-            path: '/news',
-            name: 'news',
-            component: NewsView,
-        },
-        {
-            path: '/student',
-            name: 'student',
-            component: StudentView
-        },
-        {
-            path: '/printData/:uuid',
-            name: 'printData',
+            path: '/book/:bookUUID',
+            name: 'books',
             component: DataView,
             props: true,
         },
         {
             path: '/book',
             name: 'book',
-            component: NewsView,
-            props: true,
+            component: NewsView
         }
     ]
 })
