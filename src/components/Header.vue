3033a8f7aad8c0eea16bd62b06bbf2fc2dfcefba
damouu <mouadsehbaoui@gmail.com>
2024-08-30 15:05:23 +0200

    Header.vue
    moves the FormSearch component out of the Navbar section.
    
    centers the navbar to the center of the screen.
    
    reevaluate the spaces between the icons in the navbar.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="row">
-    <nav class="navbar navbar-expand-lg navbar-light bg-success position-fixed fixed-top border border-primary">
+    <nav class="navbar navbar-expand-lg navbar-light bg-success position-fixed fixed-top">
       <ButtonCheck/>
-      <div class="collapse navbar-collapse offset-4 gx-3" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center mt-3">
+      <div class="gx-3 mx-auto" id="navbarSupportedContent">
+        <ul class="navbar-nav mb-2 mb-lg-0 text-center mt-2">
           <li class="nav-item me-3">
             <i class="bi bi-book-fill h4"></i>
             <RouterLink aria-current="page" class="nav-link active" to="/book">Books</RouterLink>
@@ -12,22 +12,22 @@
             <i class="bi bi-newspaper h4"></i>
             <RouterLink aria-current="page" class="nav-link active" to="/news">Gallery</RouterLink>
           </li>
-          <li class="nav-item dropdown me-3 ms-2">
+          <li class="nav-item dropdown me-3">
             <i class="bi bi-backpack2-fill h4"></i>
             <RouterLink aria-current="page" class="nav-link active" to="/student">Students</RouterLink>
           </li>
-          <li class="nav-item dropdown">
+          <li class="nav-item dropdown me-3">
             <i class="bi bi-journal-bookmark-fill h4"></i>
             <RouterLink aria-current="page" class="nav-link active" to="/coures">Courses</RouterLink>
           </li>
-          <li class="nav-item dropdown ms-3">
+          <li class="nav-item dropdown">
             <i class="bi bi-mortarboard-fill h4"></i>
             <RouterLink aria-current="page" class="nav-link active" to="/teachers">Teachers</RouterLink>
           </li>
         </ul>
-        <div>
-          <FormSearch/>
-        </div>
+      </div>
+      <div>
+        <FormSearch/>
       </div>
     </nav>
   </div>
