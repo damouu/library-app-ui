a73f04918bf208ddbea3892c6e90d0541237f066
damouu <mouadsehbaoui@gmail.com>
2025-09-01 22:35:13 +0200

    translate japanese

--- src/components/ButtonCheck.vue
+++ src/components/ButtonCheck.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link v-slot="{ navigate }" custom to="/">
     <button class="btn rounded ms-2 me-2" role="link" @click="navigate">
-      <i class="bi bi-chat-quote h4">LibraryApp</i>
+      <i class="bi bi-shop-window h4">図書館アプリ</i>
     </button>
   </router-link>
 </template>
