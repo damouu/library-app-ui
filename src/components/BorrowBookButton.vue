fc84382c593b4f22ba71b12af5a8dec7affbf0b9
damouu <mouadsehbaoui@gmail.com>
2025-09-01 23:20:36 +0200

    #LIBRARYVUE-42 Done Unassigned Fixed Done
    Create post borrowBook function to borrow a book by a student card uuid

--- src/components/BorrowBookButton.vue
+++ src/components/BorrowBookButton.vue
@@ -1,6 +1,17 @@
 <template>
-  <button class="btn btn-success btn-lg" type="button"><i class="bi bi-journal-bookmark-fill">&nbsp;&nbsp;Borrow</i>
+  <button @click="borrowBook" class="btn btn-success btn-lg" type="button"><i class="bi bi-journal-bookmark-fill">&nbsp;&nbsp;Borrow</i>
   </button>
 </template>
+
 <script lang="ts" setup>
+
+import {useBookStore} from "@/stores/Book";
+import {inject} from "vue";
+
+const axios: any = inject('axios');
+
+function borrowBook(): void {
+  const bookStore = useBookStore();
+  axios.post('/book/' + bookStore.book.uuid + '/studentCard/e1ff5d63-c8fb-4411-876d-dabafcfc4b7d');
+}
 </script>
