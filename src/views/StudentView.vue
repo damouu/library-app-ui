6fc1410ce0b1c9776f4f88062a4ec502e7ba8cb6
damouu <mouadsehbaoui@gmail.com>
2025-09-01 22:36:28 +0200

    changes

---
+++ src/views/StudentView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <section class="mb-4">
+      <TitleMessage title="学生の情報に関しては以下に表示されております。"/>
+    </section>
+  </div>
+  <div class="row">
+    <main class="col-12">
+      <TableList :page="pageNumber"/>
+    </main>
+  </div>
+  <div class="row">
+    <section class="mt-4">
+      <PaginationTable @pageNumberEmit="pageEmitReceived"/>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TitleMessage from "@/components/TitleMessage.vue";
+import TableList from "@/components/TableList.vue";
+import PaginationTable from "@/components/PaginationTable.vue";
+import {ref} from "vue";
+
+const pageNumber: Number = ref(0);
+
+function pageEmitReceived(page): void {
+  pageNumber.value = page;
+}
+
+</script>
+
+
