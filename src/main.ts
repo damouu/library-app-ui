21375b2f365602ef0e086bbc146343f30784672b
damouu <mouadsehbaoui@gmail.com>
2026-02-04 18:45:12 +0100

    Refactor: main.ts (#LAU-42)

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import './assets/main.css';
 import {createApp} from 'vue';
 import {createPinia} from 'pinia';
+import {createBootstrap} from 'bootstrap-vue-next'
 import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import instance from "@/plugins/axios";
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import {api} from "@/plugins/gateway";
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
-app.provide('axios', instance);
+app.use(createBootstrap);
+app.provide('axios', api);
 app.mount('#app');
