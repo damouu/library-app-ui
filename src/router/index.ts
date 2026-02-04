a7a78aadcc90da16abb3ae314e00af3bd0b2a584
damouu <mouadsehbaoui@gmail.com>
2026-02-04 18:45:00 +0100

    Refactor: index.ts (#LAU-42)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import DataView from "@/views/BookView.vue";
-import NewsView from "@/views/BookListView.vue";
-import StudentView from "@/views/StudentView.vue";
+import HomeView from '../views/home/HomeView.vue';
+import Checkout from '../views/checkout/Checkout.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,25 +11,59 @@ const router = createRouter({
             component: HomeView
         },
         {
-            path: '/news',
-            name: 'news',
-            component: NewsView,
-        },
-        {
-            path: '/student',
-            name: 'student',
-            component: StudentView
+            path: '/book',
+            name: 'book-list',
+            component: () => import('@/views/book/BookListView.vue')
         },
         {
             path: '/book/:bookUUID',
-            name: 'books',
-            component: DataView,
-            props: true,
+            name: 'book-detail',
+            component: () => import('@/views/book/BookView.vue'),
+            props: true
         },
         {
-            path: '/book',
-            name: 'book',
-            component: NewsView
+            path: '/student',
+            name: 'student-list',
+            component: () => import('@/views/user/StudentView.vue')
+        },
+        {
+            path: '/news',
+            redirect: '/book'
+        },
+
+        {
+            path: '/checkout-success',
+            name: 'checkout-success',
+            component: Checkout,
+            beforeEnter: (to, from, next) => {
+                if (!from.name) {
+                    next({name: 'home'});
+                } else {
+                    next();
+                }
+            }
+        },
+        {
+            path: '/series',
+            name: 'series',
+            component: () => import('@/views/series/seriesView.vue')
+        },
+        {
+            path: '/chapter/:chapterUuid',
+            name: 'chapter-details',
+            props: true,
+            component: () => import('@/views/book/BookView.vue')
+        },
+        {
+            path: '/series/:seriesUuid/chapters',
+            name: 'series-details',
+            props: true,
+            component: () => import('@/views/chapter/chapterView.vue')
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: () => import('@/views/error/NotFoundView.vue')
         }
     ]
 });
