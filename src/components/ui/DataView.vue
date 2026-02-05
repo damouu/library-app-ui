b7171a8ce95bd08691d8132b69c06ac743487d78
damouu <mouadsehbaoui@gmail.com>
2026-02-05 19:28:22 +0100

    Creates: DataView.vue (#LAU-40)

---
+++ src/components/ui/DataView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ route }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {inject, onMounted} from "vue";
+import {useRoute} from "vue-router";
+
+const axios = inject('axios');
+const route = useRoute().params.uuid;
+
+
+onMounted(() => {
+  axios.get('/studentIdCard/' + useRoute().params.uuid).then(response => {
+  });
+})
+
+</script>
