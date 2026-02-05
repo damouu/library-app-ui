ef74f65399751fe984e00e005f737217a8398ed8
damouu <mouadsehbaoui@gmail.com>
2026-02-05 19:28:47 +0100

    Creates: Footer.vue (#LAU-40)

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="text-center p-5 border border-primary p-2 bg-success w-100 mt-5">
+    <div class="row">
+      <div class="col">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+      </div>
+      <div class="col">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          A consectetur ea excepturi minus numquam, obcaecati rem
+          vellllit veniam. Deleniti minima mollitia nemo quae.
+        </p>
+      </div>
+      <div class="col">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis, eligendi iusto obcaecati pariatur
+          voluptatum.</p>
+      </div>
+      <div class="col">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis, eligendi iusto obcaecati pariatur
+          voluptatum.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+      </div>
+      <div class="col">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          A consectetur ea excepturi minus numquam, obcaecati rem
+          velit veniam. Deleniti minima mollitia nemo quae.
+        </p>
+      </div>
+      <div class="col">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis, eligendi iusto obcaecati pariatur
+          voluptatum.</p>
+      </div>
+      <div class="col">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis, eligendi iusto obcaecati pariatur
+          voluptatum.</p>
+      </div>
+    </div>
+  </div>
+</template>
