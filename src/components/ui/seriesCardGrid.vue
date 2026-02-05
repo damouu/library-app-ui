cc8d12b37131d318a9e4131a86e2cd0af24ade6e
damouu <mouadsehbaoui@gmail.com>
2026-02-05 19:23:26 +0100

    Creates: seriesView.vue (#LAU-30)

---
+++ src/components/ui/seriesCardGrid.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container-fluid">
+    <div class="row g-4">
+      <div
+          v-for="series in seriesStore.seriesList"
+          :key="series.uuid"
+          class="col-6 col-sm-4 col-md-3 col-lg-2 mt-4"
+      >
+        <router-link
+            :to="{ name: 'series-details', params: { seriesUuid: series.uuid }}"
+            class="text-decoration-none text-reset"
+        >
+          <div class="card h-100">
+            <img :src="series.coverArtworkUrl" class="card-img-top" alt="Cover">
+            <div class="card-body">
+              <h5 class="card-title text-primary">{{ series.title }} </h5>
+              <p class="card-text mb-1 small">サブタイトル: {{ series.author }}</p>
+              <p class="card-text mb-1 small">巻の番号: {{ series.illustrator }}</p>
+              <p class="card-text text-muted small">ページの数: {{ series.genre }}</p>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {useSeriesStore} from "@/stores/Series";
+
+const seriesStore = useSeriesStore();
+
+const props = defineProps<{
+  sort: string,
+  direction: string,
+  page: number,
+  size: number,
+}>();
+
+</script>
