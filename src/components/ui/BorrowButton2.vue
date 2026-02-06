4848973178f863368fe75ecf9ec945456d0794a1
damouu <mouadsehbaoui@gmail.com>
2026-02-06 21:12:32 +0100

    Creates: BorrowButton2.vue (#LAU-30)

---
+++ src/components/ui/BorrowButton2.vue
@@ -0,0 +1,18 @@
+<template>
+  <button
+      @click="borrow"
+      class="btn btn-primary w-100 py-3">
+    貸し出しする
+  </button>
+</template>
+
+<script setup lang="ts">
+
+import {useCartStore} from "@/stores/Cart";
+
+const cartStore = useCartStore();
+
+const borrow = cartStore.borrow;
+
+
+</script>
