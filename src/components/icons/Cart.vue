a2165f7782ce1ada59c742b69225c435845d5b05
damouu <mouadsehbaoui@gmail.com>
2026-02-09 18:46:58 +0100

    Creates: Cart.vue (#LAU-42)

---
+++ src/components/icons/Cart.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="btn rounded">
+    <i class="bi bi-cart-fill h2"> <span class="text-danger"> {{ cartStore.items.length }}</span> </i>
+  </button>
+</template>
+
+<script setup lang="ts">
+import {useCartStore} from "@/stores/Cart";
+
+const cartStore = useCartStore();
+
+</script>
