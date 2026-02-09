147a8fc83d055d6300b212ba81860198f986528e
damouu <mouadsehbaoui@gmail.com>
2026-02-09 18:47:56 +0100

    Creates: CheckoutInfo.vue (#LAU-42)

---
+++ src/components/ui/CheckoutInfo.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>以下の{{ cartStore.items.length }}冊を貸し出しで宜しでしょうか</h3>
+</template>
+
+<script lang="ts" setup>
+
+import {useCartStore} from "@/stores/Cart";
+
+const cartStore = useCartStore();
+
+</script>
