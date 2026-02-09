c2d8fa21b8c7bcfd39fff5de4139cb244d9a986e
damouu <mouadsehbaoui@gmail.com>
2026-02-09 18:46:15 +0100

    Creates: AuthActions.vue (#LAU-42)

---
+++ src/components/user/AuthActions.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex gap-2">
+    <button class="btn btn-outline-dark btn-sm" @click="goToLogin">
+      サインイン
+    </button>
+    <button class="btn btn-dark btn-sm" @click="goToRegister">
+      新規登録
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {useRouter} from 'vue-router';
+
+const router = useRouter();
+
+const goToLogin = () => router.push('/login');
+const goToRegister = () => router.push('/register');
+</script>
