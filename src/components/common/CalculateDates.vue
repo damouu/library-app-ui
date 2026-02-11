195ea87d13263d8945818b545b6ae8205fedbdc1
damouu <mouadsehbaoui@gmail.com>
2026-02-11 20:27:03 +0100

    Refactor: CalculateDates.vue (#LAU-25)

---
+++ src/components/common/CalculateDates.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container text-center">
+    <div class="row align-items-center">
+      <h3>現在の日付: {{ currentDateFormated }} &nbsp;&nbsp; 予想返却期限日: {{ dedeFormated }}</h3>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+const formatDate = (date: any): string => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+};
+
+
+const currentDate = new Date();
+const currentDateFormated = formatDate(currentDate);
+
+const dede = currentDate;
+dede.setDate(dede.getDate() + 14);
+
+const dedeFormated = formatDate(dede);
+
+
+</script>
+
