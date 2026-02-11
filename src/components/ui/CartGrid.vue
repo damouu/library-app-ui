e72b028c9fa4c930e7d8d55c4e5c92d36b9ec566
damouu <mouadsehbaoui@gmail.com>
2026-02-11 20:29:24 +0100

    Create: CartGrid.vue (#LAU-25)

---
+++ src/components/ui/CartGrid.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="row">
+      <div class="col-12  mt-3">
+        <div class="row g-3 justify-content-center">
+          <div v-for="item in cartStore.lastTransaction.borrowedItems" :key="item.book_uuid" class="col-2">
+            <img :src="item.chapter.chapter_cover_url" class="img-fluid rounded shadow-sm">
+            <p class="small mt-1">{{ item.chapter.chapter_title }} {{ item.chapter.chapter_number }}巻</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {useCartStore} from "@/stores/Cart";
+
+const cartStore = useCartStore();
+
+
+</script>
