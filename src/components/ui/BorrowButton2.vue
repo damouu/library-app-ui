31acff11612ed9d604973923dda481c5e2950473
damouu <mouadsehbaoui@gmail.com>
2026-02-11 20:26:35 +0100

    Refactor: BorrowButton2.vue (#LAU-25)

--- src/components/ui/BorrowButton2.vue
+++ src/components/ui/BorrowButton2.vue
@@ -1,18 +1,36 @@
 <template>
-  <button
-      @click="borrow"
-      class="btn btn-primary w-100 py-3">
-    貸し出しする
-  </button>
+  <div>
+    <button
+        :disabled="cartStore.isLoading"
+        @click="handleBorrowAction"
+        class="btn btn-warning py-3 shadow-sm"
+        style="min-width: 250px;"
+    >
+      <span v-if="cartStore.isLoading" class="spinner-border spinner-border-sm"></span>
+      <span v-else>貸し出しする</span>
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
 
 import {useCartStore} from "@/stores/Cart";
+import router from "@/router";
 
 const cartStore = useCartStore();
 
-const borrow = cartStore.borrow;
+const handleBorrowAction = async () => {
+
+  const borrow = await cartStore.borrow();
+
+  if (borrow) {
+    await router.push({
+      name: 'checkout'
+    });
+  } else {
+    alert("エラーが発生しました。もう一度お試しください。");
+  }
+};
 
 
 </script>
