3b73fcd53803170f367277269a941e613472a905
damouu <mouadsehbaoui@gmail.com>
2026-02-11 20:30:27 +0100

    Refactor: seriesView.vue (#LAU-25)

--- src/views/series/seriesView.vue
+++ src/views/series/seriesView.vue
@@ -7,7 +7,9 @@
           :page="0"
           :size="6"
           sort="firstPrintPublicationDate"
-          direction="desc" v-else/>
+          direction="desc" v-else
+          @change-page="handlePageChange"
+      />
     </div>
   </main>
 </template>
@@ -16,11 +18,28 @@
 import SeriesCardGrid from '../../components/ui/seriesCardGrid.vue'
 import {useSeriesStore} from "@/stores/Series";
 import {onMounted} from "vue";
+import {useRoute, useRouter} from 'vue-router';
 
+const router = useRouter();
+const route = useRoute();
 const seriesStore = useSeriesStore();
 
+async function handlePageChange(newPage: number) {
+
+  await router.push({query: {...route.query, page: newPage + 1}});
+
+  await seriesStore.getSeries(newPage, 12, 'firstPrintPublicationDate', 'desc');
+}
+
 onMounted(async () => {
-  await seriesStore.getSeries(0, 6, "firstPrintPublicationDate", "desc");
+  const pageFromUrl = route.query.page ? Number(route.query.page) : 1;
+
+  if (!route.query.page) {
+    await router.replace({query: {...route.query, page: 1}});
+  }
+
+  await seriesStore.getSeries(pageFromUrl - 1, 12, "firstPrintPublicationDate", "desc");
 });
+
 
 </script>
