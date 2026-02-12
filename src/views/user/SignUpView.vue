b7495482a78c1c0a81b5f60b59246bbe845b9b25
damouu <mouadsehbaoui@gmail.com>
2026-02-12 13:44:43 +0100

    Refactor: SignUpView.vue (#LAU-12)

---
+++ src/views/user/SignUpView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="studentStore.student && useRoute().params.userUUID">
+    <div class="row">
+      <section class="mb-4">
+        <UserData/>
+      </section>
+    </div>
+  </div>
+  <div v-else>
+    <p>not allowed</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import UserData from "@/components/user/UserData.vue";
+import useStudentStore from "@/stores/User";
+import {onBeforeMount} from "vue";
+import {useRoute} from "vue-router";
+
+const studentStore = useStudentStore();
+
+onBeforeMount(() => {
+  studentStore.getStudentIdCard(useRoute().params.userUUID);
+
+})
+
+</script>
