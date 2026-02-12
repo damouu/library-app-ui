58f54eacad9842b42df52b4c8021da5adbf9e78e
damouu <mouadsehbaoui@gmail.com>
2026-02-12 13:44:35 +0100

    Refactor: SignInView.vue (#LAU-12)

---
+++ src/views/user/SignInView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="align-items-center justify-content-center offset-4">
+    <SignInForm />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SignInForm from '@/components/user/signIn.vue';
+</script>
