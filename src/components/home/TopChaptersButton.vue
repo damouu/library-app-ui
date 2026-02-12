b7c0639d10304daf813dc2c8f22f1989f6f90f8d
damouu <mouadsehbaoui@gmail.com>
2026-02-12 13:45:18 +0100

    Refactor: TopChaptersButton.vue (#LAU-12)

---
+++ src/components/home/TopChaptersButton.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="row justify-content-center mt-4">
+    <div class="col-3"></div>
+
+    <div class="col-6 text-center">
+      <h2 class="m-0">
+        <i class="bi bi-trophy-fill"> ランキング</i>
+      </h2>
+    </div>
+
+    <div class="col-3 d-flex justify-content-end">
+      <div class="btn-group" role="group">
+        <button
+            type="button"
+            class="btn btn-outline-primary"
+            :class="{ active: period === 'week' }"
+            @click="updatePeriod('week')"
+        >
+          今週
+        </button>
+        <button
+            type="button"
+            class="btn btn-outline-primary"
+            :class="{ active: period === 'lastweek' }"
+            @click="updatePeriod('lastweek')"
+        >
+          先週
+        </button>
+        <button
+            type="button"
+            class="btn btn-outline-primary"
+            :class="{ active: period === 'lastmonth' }"
+            @click="updatePeriod('lastmonth')"
+        >
+          先月
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import {ref} from 'vue';
+
+type PeriodKey = 'week' | 'lastweek' | 'lastmonth';
+
+const period = ref<PeriodKey>('week');
+
+const emit = defineEmits<{
+  (e: 'period', value: PeriodKey): void
+}>();
+
+function updatePeriod(newPeriod: PeriodKey) {
+  period.value = newPeriod;
+  emit('period', newPeriod);
+}
+
+
+</script>
