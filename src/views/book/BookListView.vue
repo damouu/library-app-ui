0dddb154968375c2ad1dec468c376bd91c13b805
damouu <mouadsehbaoui@gmail.com>
2026-02-12 13:50:04 +0100

    Create: BookListView.vue (#LAU-12)

---
+++ src/views/book/BookListView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-fluid px-0">
+    <section>
+      <main>
+        <CardGrid/>
+      </main>
+    </section>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+import CardGrid from "@/components/ui/CardGrid.vue"
+import {ref} from "vue";
+
+let inputReceived: String = ref(null);
+let booksNotFound: Boolean = ref(null);
+
+function pageEmitReceived(inputEmit): void {
+  inputReceived.value = inputEmit;
+}
+
+function bookEmitReceived(bookEmit): void {
+  booksNotFound.value = bookEmit;
+}
+
+</script>
