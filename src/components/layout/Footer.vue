d3b82ea3f0fe431b1a9e45dfcf6ee74ba69ffcfd
damouu <mouadsehbaoui@gmail.com>
2026-02-12 13:43:21 +0100

    Refactor: Footer.vue (#LAU-12)

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,43 +1,18 @@
 <template>
-  <div class="text-center p-5 border border-primary p-2 bg-success w-100 mt-5">
+  <div class="text-center p-5 border border-primary p-2 bg-warning w-100 mt-5">
     <div class="row">
       <div class="col">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
       </div>
       <div class="col">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          A consectetur ea excepturi minus numquam, obcaecati rem
-          vellllit veniam. Deleniti minima mollitia nemo quae.
-        </p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
       </div>
       <div class="col">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis, eligendi iusto obcaecati pariatur
-          voluptatum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
       </div>
       <div class="col">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis, eligendi iusto obcaecati pariatur
-          voluptatum.</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-      </div>
-      <div class="col">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          A consectetur ea excepturi minus numquam, obcaecati rem
-          velit veniam. Deleniti minima mollitia nemo quae.
-        </p>
-      </div>
-      <div class="col">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis, eligendi iusto obcaecati pariatur
-          voluptatum.</p>
-      </div>
-      <div class="col">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis, eligendi iusto obcaecati pariatur
-          voluptatum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
       </div>
     </div>
   </div>
