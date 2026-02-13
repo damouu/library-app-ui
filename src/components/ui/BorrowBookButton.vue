f351c56f13a3c54739e2cd0b0d030f1611566e76
damouu <mouadsehbaoui@gmail.com>
2026-02-13 19:51:43 +0100

    Create: BorrowBookButton.vue (#LAU-12)

---
+++ src/components/ui/BorrowBookButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="borrowBook" class="btn btn-success btn-lg" type="button"><i class="bi bi-journal-bookmark-fill">&nbsp;&nbsp;Borrow</i>
+  </button>
+</template>
+
+<script lang="ts" setup>
+
+import {useBookStore} from "@/stores/Book";
+import {inject} from "vue";
+
+const axios: any = inject('axios');
+
+function borrowBook(): void {
+  const bookStore = useBookStore();
+  axios.post('/book/' + bookStore.book.uuid + '/studentIdCard/e1ff5d63-c8fb-4411-876d-dabafcfc4b7d');
+}
+</script>
