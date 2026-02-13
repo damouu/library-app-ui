76a4bac7a04940c3da84b466b68955d472f5b7c7
damouu <mouadsehbaoui@gmail.com>
2026-02-13 19:51:20 +0100

    Create: BooksView.vue (#LAU-12)

---
+++ src/views/book/BooksView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container-fluid px-0">
+    <section>
+      <main>
+        <CardGrid/>
+      </main>
+    </section>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import CardGrid from "@/components/ui/CardGrid.vue";
+</script>
