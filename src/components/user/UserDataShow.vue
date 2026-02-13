6ebec8c55bdf98f43b5d697c28e2f9a8de33beb7
damouu <mouadsehbaoui@gmail.com>
2026-02-13 19:53:28 +0100

    Create: UserData.vue (#LAU-12)

---
+++ src/components/user/UserDataShow.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3 class="text-primary">User Card data </h3>
+  <br>
+
+
+  <h5 class="text-success">{{ bookStore.book.studentIdCard }}</h5>
+</template>
+
+<script lang="ts" setup>
+import {useBookStore} from "@/stores/Book";
+
+const bookStore = useBookStore();
+
+</script>
