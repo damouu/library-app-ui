214ae60017b24ff7dd1772e321127b27fa62f007
damouu <mouadsehbaoui@gmail.com>
2026-02-16 20:09:08 +0100

    Create: FilterButton.vue (#LAU-25)

--- src/components/ui/FilterButton.vue
+++ src/components/ui/FilterButton.vue
@@ -1,6 +1,29 @@
 <template>
-  <button class="btn btn-dark btn-lg" type="button"><i class="bi bi-filter">&nbsp;&nbsp;Filters</i>
+  <button class="btn btn-primary" type="button" @click="openFilterModal">
+    <i class="bi bi-filter"> 検索条件</i>
   </button>
+
+  <SeriesFilterModal
+      v-if="showModal"
+      @close="showModal = false"
+      @confirm="emitFilter"
+  />
 </template>
+
 <script lang="ts" setup>
+import {ref} from "vue";
+import SeriesFilterModal from "@/components/ui/SeriesFilterModal.vue";
+
+const emit = defineEmits(['confirm']);
+const showModal = ref(false);
+
+const openFilterModal = () => {
+  showModal.value = true;
+};
+
+const emitFilter = (filterData: any) => {
+  emit('confirm', filterData);
+  showModal.value = false;
+};
+
 </script>
