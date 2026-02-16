19c84f18c192b16db7151aab9fc77f4067ed9ec2
damouu <mouadsehbaoui@gmail.com>
2026-02-16 20:09:16 +0100

    Create: FilterChapterButton.vue (#LAU-25)

---
+++ src/components/ui/FilterChapterButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="btn btn-primary" type="button" @click="openFilterModal">
+    <i class="bi bi-filter"> 検索条件</i>
+  </button>
+
+  <ChapterFilterModal
+      v-if="showModal"
+      @close="showModal = false"
+      @confirm="emitFilter"
+  />
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue";
+import ChapterFilterModal from "@/components/ui/ChapterFilterModal.vue";
+
+const emit = defineEmits(['confirm']);
+const showModal = ref(false);
+
+const openFilterModal = () => {
+  showModal.value = true;
+};
+
+const emitFilter = (filterData: any) => {
+  emit('confirm', filterData);
+  showModal.value = false;
+};
+
+</script>
