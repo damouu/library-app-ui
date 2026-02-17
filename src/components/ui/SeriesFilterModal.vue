4c915e4491794b2f1a36acbd1146903738faf43c
damouu <mouadsehbaoui@gmail.com>
2026-02-17 16:13:02 +0100

    Create: SeriesFilterModal.vue (#LAU-39)

---
+++ src/components/ui/SeriesFilterModal.vue
@@ -0,0 +1,77 @@
+<template>
+  <div
+      class="modal fade show d-block"
+      style="background-color: rgba(0, 0, 0, 0.5);"
+      tabindex="-1"
+      aria-modal="true"
+      role="dialog"
+      @click.self="$emit('close')"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content border-0 shadow">
+        <div class="modal-header bg-light">
+          <h5 class="modal-title fw-bold">絞り込み検索</h5>
+          <button type="button" class="btn-close" @click="$emit('close')"></button>
+        </div>
+
+        <div class="modal-body p-4">
+          <form @submit.prevent="applyFilters">
+            <div class="mb-3">
+              <label class="form-label fw-semibold">タイトル</label>
+              <input v-model="filters.title" type="text" class="form-control" placeholder="例: 呪術廻戦">
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label fw-semibold">著者</label>
+              <input v-model="filters.author" type="text" class="form-control" placeholder="著者名を入力">
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label fw-semibold">作画</label>
+              <input v-model="filters.illustrator" type="text" class="form-control" placeholder="作画担当者名を入力">
+            </div>
+
+            <div class="row">
+              <div class="col-md-6 mb-3">
+                <label class="form-label fw-semibold">ジャンル</label>
+                <input v-model="filters.genre" type="text" class="form-control" placeholder="ファンタジーなど">
+              </div>
+
+              <div class="col-md-6 mb-3">
+                <label class="form-label fw-semibold">出版社</label>
+                <input v-model="filters.publisher" type="text" class="form-control" placeholder="集英社など">
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div class="modal-footer border-top-0 px-4 pb-4">
+          <button type="button" class="btn btn-outline-secondary px-4" @click="resetFilters">リセット</button>
+          <button type="button" class="btn btn-primary px-4" @click="applyFilters">検索する</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const emit = defineEmits(['close', 'confirm']);
+
+const filters = ref({
+  title: '', author: '', illustrator: '', genre: '', publisher: ''
+});
+
+const applyFilters = () => {
+  const cleanFilters = Object.fromEntries(
+      Object.entries(filters.value).filter(([_, value]) => value !== '')
+  );
+
+  emit('confirm', cleanFilters);
+};
+
+const resetFilters = () => {
+  filters.value = {title: '', author: '', illustrator: '', genre: '', publisher: ''};
+};
+</script>
