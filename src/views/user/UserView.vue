f1a9e9e849819cb9abd00d8f965631fe639ae65b
damouu <mouadsehbaoui@gmail.com>
2026-02-17 16:12:28 +0100

    Create: UserView.vue (#LAU-39)

---
+++ src/views/user/UserView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container mt-5 pt-5 ">
+    <div class="row g-lg-5">
+      <div class="col-md-4 mb-4">
+        <UserProfile/>
+      </div>
+
+      <div class="col-md-8">
+        <div class="row g-lg-7 justify-content-center">
+          <h4 class="mb-4">最近の貸出履歴</h4>
+        </div>
+        <BorrowHistory/>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BorrowHistory from '@/components/user/BorrowHistory.vue';
+import UserProfile from '@/components/user/UserProfile.vue';
+</script>
