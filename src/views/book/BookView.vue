699c610a23609bdb6af0109ee6397276fee20c16
damouu <mouadsehbaoui@gmail.com>
2026-02-23 16:13:50 +0100

    Refactor: BookView.vue (#LAU-22)

--- src/views/book/BookView.vue
+++ src/views/book/BookView.vue
@@ -30,18 +30,52 @@
 
     </div>
 
-    <div>
-      <Comments :chapterUuid="props.chapterUuid" , :page="0" :size="5" class="mt-5"/>
+    <div class="mt-5">
+
+      <Comments
+          :chapterUuid="props.chapterUuid"
+          :page="page"
+          :size="5"
+      />
+
+      <template v-if="!commentStore.isLoading">
+
+        <div v-if="commentStore.pagination.totalPages > 1" class="mt-4">
+
+          <Pagination
+              :total-pages="commentStore.pagination.totalPages"
+              :current-page="commentStore.pagination.currentPage"
+              :is-first="commentStore.pagination.isFirst"
+              :is-last="commentStore.pagination.isLast"
+              @change-page="handlePageChange"
+          />
+
+        </div>
+
+        <div
+            v-else-if="commentStore.commentsList?.length === 0"
+            class="text-center py-5 border rounded-3 bg-light mt-4"
+        >
+          <i class="bi bi-chat-left-dots text-muted fs-1"></i>
+          <p class="mt-2 text-secondary fw-semibold">まだコメントはありません</p>
+          <p class="small text-muted">最初の感想をシェアしませんか？</p>
+        </div>
+
+      </template>
     </div>
 
-    <div v-if="commentStore.commentsList?.length > 0" class="mt-4">
-
-      <Pagination
-          :total-pages="commentStore.pagination.totalPages"
-          :current-page="commentStore.pagination.currentPage"
-          :is-first="commentStore.pagination.isFirst"
-          :is-last="commentStore.pagination.isLast"
+    <div class="mt-4">
+      <CommentInput
+          v-if="userStore.currentUser?.name"
+          :chapterUuid="props.chapterUuid"
       />
+
+      <div v-else class="p-4 border rounded-3 bg-light text-center">
+        <p class="text-muted mb-3">コメントを投稿するにはログインが必要です</p>
+        <router-link to="/login" class="btn btn-outline-primary">
+          <i class="bi bi-box-arrow-in-right me-2"></i>ログインする
+        </router-link>
+      </div>
     </div>
 
   </div>
@@ -50,21 +84,32 @@
 <script lang="ts" setup>
 import ResourceDataShow from "@/components/book/RessourceDataShow.vue";
 import Comments from "@/components/ui/Comment.vue";
+import CommentInput from "@/components/ui/CommentPost.vue";
 import Pagination from "../../components/common/Pagination.vue"
 import {useCommentStore} from "@/stores/Comment";
+import {useUserStore} from "@/stores/User";
 import {useInventoryStore} from "@/stores/Inventory";
 import {useCartStore} from "@/stores/Cart";
-import {onMounted, onUnmounted} from "vue";
+import {onMounted, onUnmounted, ref} from "vue";
 import CopiesAvailable from "@/components/book/CopieAvailable.vue";
 import NoCopiesAvailable from "@/components/book/NoCopieAvailable.vue";
 
 const commentStore = useCommentStore();
 const inventoryStore = useInventoryStore();
+const userStore = useUserStore();
 const cartStore = useCartStore();
+const page = ref<number>(0);
 
 const props = defineProps<{
   chapterUuid: string
 }>();
+
+
+async function handlePageChange(newPage: number) {
+  page.value = newPage;
+
+  await commentStore.getChapter(newPage + 1, 5, props.chapterUuid);
+}
 
 onMounted(() => {
   inventoryStore.checkAvailability(props.chapterUuid);
