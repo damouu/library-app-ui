f3fc980f2edb45ac9bf178b24d1ca7219103a723
damouu <mouadsehbaoui@gmail.com>
2026-03-25 22:49:30 +0100

    Create: SeriesFilterModal.vue (#LAU-10)

--- src/components/ui/SeriesFilterModal.vue
+++ src/components/ui/SeriesFilterModal.vue
@@ -15,30 +15,31 @@
         </div>
 
         <div class="modal-body p-4">
-          <form @submit.prevent="applyFilters">
-            <div class="mb-3">
-              <label class="form-label fw-semibold">タイトル</label>
+          <form @submit.prevent="applyFilters" @keyup.enter="applyFilters">
+
+            <div class="mb-3 text-start">
+              <label class="form-label fw-semibold d-block">タイトル</label>
               <input v-model="filters.title" type="text" class="form-control" placeholder="例: 呪術廻戦">
             </div>
 
-            <div class="mb-3">
-              <label class="form-label fw-semibold">著者</label>
+            <div class="mb-3 text-start">
+              <label class="form-label fw-semibold d-block">著者</label>
               <input v-model="filters.author" type="text" class="form-control" placeholder="著者名を入力">
             </div>
 
-            <div class="mb-3">
-              <label class="form-label fw-semibold">作画</label>
+            <div class="mb-3 text-start">
+              <label class="form-label fw-semibold d-block">作画</label>
               <input v-model="filters.illustrator" type="text" class="form-control" placeholder="作画担当者名を入力">
             </div>
 
             <div class="row">
-              <div class="col-md-6 mb-3">
-                <label class="form-label fw-semibold">ジャンル</label>
+              <div class="col-md-6 mb-3 text-start">
+                <label class="form-label fw-semibold d-block">ジャンル</label>
                 <input v-model="filters.genre" type="text" class="form-control" placeholder="ファンタジーなど">
               </div>
 
-              <div class="col-md-6 mb-3">
-                <label class="form-label fw-semibold">出版社</label>
+              <div class="col-md-6 mb-3 text-start">
+                <label class="form-label fw-semibold d-block">出版社</label>
                 <input v-model="filters.publisher" type="text" class="form-control" placeholder="集英社など">
               </div>
             </div>
@@ -46,8 +47,8 @@
         </div>
 
         <div class="modal-footer border-top-0 px-4 pb-4">
-          <button type="button" class="btn btn-outline-secondary px-4" @click="resetFilters">リセット</button>
-          <button type="button" class="btn btn-primary px-4" @click="applyFilters">検索する</button>
+          <button type="button" class="btn btn-outline-secondary px-4 rounded-pill" @click="resetFilters">リセット</button>
+          <button type="button" class="btn btn-primary px-4 rounded-pill" @click="applyFilters">検索する</button>
         </div>
       </div>
     </div>
@@ -55,12 +56,28 @@
 </template>
 
 <script setup>
-import {ref} from 'vue';
+import {onMounted, ref} from 'vue';
+import {useRoute} from 'vue-router';
 
+const route = useRoute();
 const emit = defineEmits(['close', 'confirm']);
 
+
 const filters = ref({
-  title: '', author: '', illustrator: '', genre: '', publisher: ''
+  title: route.query.title || '',
+  author: route.query.author || '',
+  illustrator: route.query.illustrator || '',
+  genre: route.query.genre || '',
+  publisher: route.query.publisher || ''
+});
+
+
+onMounted(() => {
+  filters.value.title = route.query.title || '';
+  filters.value.author = route.query.author || '';
+  filters.value.illustrator = route.query.illustrator || '';
+  filters.value.genre = route.query.genre || '';
+  filters.value.publisher = route.query.publisher || '';
 });
 
 const applyFilters = () => {
@@ -69,6 +86,7 @@ const applyFilters = () => {
   );
 
   emit('confirm', cleanFilters);
+  emit('close');
 };
 
 const resetFilters = () => {
