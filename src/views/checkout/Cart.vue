83a3f7b80c6d33125b0b4b64e03a8614e680f830
damouu <mouadsehbaoui@gmail.com>
2026-03-29 15:01:31 +0200

    Refactor: Cart.vue (#LAU-10)

--- src/views/checkout/Cart.vue
+++ src/views/checkout/Cart.vue
@@ -1,22 +1,21 @@
 <template>
   <div class="container py-4">
-    <div class="row">
+    <div class="row g-2">
       <div class="col-12">
         <Calculator/>
       </div>
-      <div class="col-12 mt-3">
-        <CheckoutInfo class="mt-3 text-center"/>
-      </div>
-    </div>
 
-    <div class="row">
-      <div class="col-12 mb-5 mt-5">
+      <div class="col-12">
+        <CheckoutInfo class="text-center"/>
+      </div>
+
+      <div class="col-12">
         <CheckoutGrid/>
       </div>
     </div>
 
     <div class="row mt-1">
-      <div class="col-12 d-flex justify-content-center">
+      <div class="col-12 d-flex justify-content-center mt-5">
         <BorrowButton/>
       </div>
     </div>
