2ae194462cc5fba06f12610c1b242c91e50512ea
damouu <mouadsehbaoui@gmail.com>
2026-04-13 13:07:21 +0900

    Refactor: LogoComponent.vue (#LAU-10)

---
+++ src/components/ui/LogoComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <RouterLink to="/" class="logo-link d-flex align-items-center text-decoration-none">
+    <div class="d-flex align-items-center gap-3">
+
+      <div class="logo-icon">
+        <i class="bi bi-book-fill"></i>
+      </div>
+
+      <div class="d-flex flex-column">
+        <span class="logo-main">図書館アプリ</span>
+        <span class="logo-sub">Library App</span>
+      </div>
+    </div>
+  </RouterLink>
+</template>
+
+<script setup lang="ts">
+import {RouterLink} from 'vue-router';
+</script>
