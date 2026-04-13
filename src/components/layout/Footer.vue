3241406951bb260512ab37b9cdce94610cd73268
damouu <mouadsehbaoui@gmail.com>
2026-04-13 13:08:56 +0900

    Refactor: Footer.vue (#LAU-10)

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="text-center p-5 border border-primary p-2 bg-warning w-100 mt-5">
+  <div class="text-center p-5 border border-primary w-100 mt-5"
+       style="background: linear-gradient(135deg, #1e3a8a 0%, #3b82f6 100%); color: white;">
     <div class="row">
-      <div class="col">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+
+      <div class="col-md-4 mb-3">
+        <h6 class="fw-bold mb-2">図書館アプリ</h6>
+        <p class="small opacity-75">
+          マイクロサービスとイベント駆動設計をベースにした
+          Webアプリケーション
+        </p>
       </div>
-      <div class="col">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+
+      <div class="col-md-4 mb-3">
+        <h6 class="fw-bold mb-2">技術スタック</h6>
+        <p class="small opacity-75">
+          Spring Boot / Laravel / Node.js<br>
+          PostgreSQL / MongoDB / Redis<br>
+          Kafka / Docker / OpenTelemetry
+        </p>
       </div>
-      <div class="col">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+
+      <div class="col-md-4 mb-3">
+        <h6 class="fw-bold mb-2">エンジニア</h6>
+        <p class="small opacity-75">
+          バックエンドエンジニア（2年）<br>
+          日本語能力試験 N1<br>
+          東京での開発経験あり
+        </p>
       </div>
-      <div class="col">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-      </div>
+
     </div>
   </div>
 </template>
