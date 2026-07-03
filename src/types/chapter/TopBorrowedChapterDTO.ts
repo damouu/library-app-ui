d0dc2851c0fdbe030428e335c365379a644b0bec
damouu <mouadsehbaoui@gmail.com>
2026-07-03 19:52:43 +0900

    Create: TopBorrowedChapterDTO.ts (#LAU-43)

---
+++ src/types/chapter/TopBorrowedChapterDTO.ts
@@ -0,0 +1,8 @@
+export interface TopBorrowedChapterDTO {
+    chapterUuid: string;
+    borrowCount: number;
+    title: string;
+    secondTitle: string;
+    chapterNumber: number;
+    coverArtworkUrl: string;
+}
