2d152d1affee65cb7122edaaa09aa097aa65e180
damouu <mouadsehbaoui@gmail.com>
2026-07-03 19:51:24 +0900

    Create: ProfileResponse.ts (#LAU-43)

---
+++ src/types/auth/ProfileResponse.ts
@@ -0,0 +1,14 @@
+import {User} from "@/models/User";
+import type {UserResponse} from "@/types/auth/UserResponse";
+
+
+
+export function mapUser(response: UserResponse): User {
+    return new User(
+        response.card_uuid,
+        response.user_name,
+        response.email,
+        response.avatar_img_url,
+        response.last_logged_in_at
+    );
+}
