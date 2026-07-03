fbd1def95551464cda8e05d79c41eed4cb9efda4
damouu <mouadsehbaoui@gmail.com>
2026-07-03 19:52:32 +0900

    Create: TopBorrowedChapter.ts (#LAU-43)

---
+++ src/types/chapter/TopBorrowedChapter.ts
@@ -0,0 +1,8 @@
+export interface TopBorrowedChapter {
+    chapterUuid: string;
+    borrowCount: number;
+    title: string;
+    secondTitle: string;
+    chapterNumber: number;
+    coverArtworkUrl: string;
+}
