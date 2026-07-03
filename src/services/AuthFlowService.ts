5927ef416122b627b88f160284d82db9a4fe3ef3
damouu <mouadsehbaoui@gmail.com>
2026-07-03 19:47:39 +0900

    Create: AuthFlowService.ts (#LAU-43)

---
+++ src/services/AuthFlowService.ts
@@ -0,0 +1,18 @@
+import {AuthService} from "@/services/AuthService";
+import {RecordService} from "@/services/RecordService";
+
+export class AuthFlowService {
+
+    static async login(email: string, password: string) {
+
+        const {user, token} = await AuthService.signIn(email, password);
+
+        const records = await RecordService.getRecords(0, 5);
+
+        return {
+            user,
+            token,
+            records
+        };
+    }
+}
