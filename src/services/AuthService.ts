3f6fef5184d7e7e5278ac6daad867400a2934931
damouu <mouadsehbaoui@gmail.com>
2026-07-03 19:47:49 +0900

    Create: AuthService.ts (#LAU-43)

---
+++ src/services/AuthService.ts
@@ -0,0 +1,40 @@
+import {api} from "@/plugins/gateway";
+import {mapUserJwt} from "@/mappers/UserJwtMapper";
+import {decodeJwt} from "@/utils/Jwt";
+import type {UserJwt} from "@/types/auth/UserJwt";
+
+export class AuthService {
+
+    static async signIn(email: string, password: string) {
+
+        const base64Credentials = window.btoa(`${email}:${password}`);
+
+        const response = await api.post("/api/auth/login", {}, {
+            headers: {
+                Authorization: `Basic ${base64Credentials}`
+            }
+        });
+
+        const token = response.data.access_token;
+        console.log(token);
+
+        const payload = decodeJwt<UserJwt>(token);
+
+        return {token, user: mapUserJwt(payload)};
+    }
+
+
+    static async signUp(user_name: string, email: string, password: string, password_confirmation: string) {
+
+        const response = await api.post(`/api/auth/register`, {
+            user_name, email, password, password_confirmation
+        }, {});
+
+        const token = response.data.access_token;
+
+        const payload = decodeJwt<UserJwt>(token);
+
+        return {token, user: mapUserJwt(payload)};
+
+    }
+}
