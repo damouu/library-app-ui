6fd6b0570102de3dfe2219bb4bd17d29edd66d48
damouu <mouadsehbaoui@gmail.com>
2026-07-03 19:49:43 +0900

    Create: ChapterMapper.ts (#LAU-43)

---
+++ src/mappers/ChapterMapper.ts
@@ -0,0 +1,18 @@
+import type {ChapterDTO} from "@/types/chapter/ChapterDTO";
+import {Chapter} from "@/models/Chapter";
+import {mapSeries} from "@/mappers/SeriesMapper";
+
+
+export function mapChapter(dto: ChapterDTO): Chapter {
+    return new Chapter(
+        dto.uuid,
+        dto.title,
+        dto.secondTitle,
+        dto.totalPages,
+        dto.chapterNumber,
+        dto.coverArtworkUrl,
+        dto.summary,
+        dto.publicationDate,
+        mapSeries(dto.series)
+    );
+}
