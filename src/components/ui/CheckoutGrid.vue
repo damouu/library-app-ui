bb61decadaa615e2704e9e259334aae1bdaa9076
damouu <mouadsehbaoui@gmail.com>
2026-07-04 17:46:46 +0900

    Refactor: CheckoutGrid.vue (#LAU-43)

--- src/components/ui/CheckoutGrid.vue
+++ src/components/ui/CheckoutGrid.vue
@@ -2,21 +2,21 @@
   <div class="row g-4 justify-content-center mb-3 mt-3">
     <div
         v-for="item in cartStore.items"
-        :key="item.book_uuid"
+        :key="item.bookUuid"
         class="col-6 col-sm-4 col-md-3 col-lg-2 mt-4"
     >
 
       <div class="card h-95">
-        <img :src="item.chapter.chapter_cover_url" class="card-img-top" alt="Cover">
+        <img :src="item.chapter.coverArtworkUrl" class="card-img-top" alt="Cover">
         <div class="card-body">
-          <h5 class="card-title text-primary text-center">{{ item.chapter.chapter_title }} </h5>
-          <p class="card-text mb-1 text-center">{{ item.chapter.chapter_second_title }}</p>
-          <p class="card-text mb-1 text-center">巻 {{ item.chapter.chapter_number }}</p>
+          <h5 class="card-title text-primary text-center">{{ item.chapter.title }} </h5>
+          <p class="card-text mb-1 text-center">{{ item.chapter.secondTitle }}</p>
+          <p class="card-text mb-1 text-center">巻 {{ item.chapter.chapterNumber }}</p>
         </div>
       </div>
 
       <button
-          @click="cartStore.removeFromCart(item.book_uuid)"
+          @click="cartStore.removeFromCart(item.bookUuid)"
           class="btn btn-danger btn-lg btn-sm text-truncate mt-2 w-100 py-2 mt-4"
       >
         <i class="bi-trash3"/>
