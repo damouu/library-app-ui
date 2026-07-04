6139016f04bcf5856e974f3d6f0e15911c465619
damouu <mouadsehbaoui@gmail.com>
2026-07-04 17:47:34 +0900

    Refactor: RecordService.ts (#LAU-43)

--- src/services/RecordService.ts
+++ src/services/RecordService.ts
@@ -4,8 +4,11 @@ import type {RecordResponse} from "@/types/records/RecordResponse";
 
 export class RecordService {
 
-    static async getRecords(page: number, size: number): Promise<RecordResponse> {
+    static async getRecords(page: number, size: number, token: string): Promise<RecordResponse> {
         const response = await api.get("/api/records", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
             params: {page, size}
         });
         return mapRecords(response.data);
