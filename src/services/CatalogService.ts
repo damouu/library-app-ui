66479ca60ef15d27f3a70896f922b87c71a9a74b
damouu <mouadsehbaoui@gmail.com>
2026-07-04 17:46:22 +0900

    Refactor: CatalogService.ts (#LAU-43)

--- src/services/CatalogService.ts
+++ src/services/CatalogService.ts
@@ -1,10 +1,14 @@
 import {api} from "@/plugins/gateway";
-import type {ChapterPageDTO} from "@/types/chapter/ChapterPageDTO";
-import {mapChapterPage} from "@/mappers/ChapterPageMapper";
-import type {ChapterPage} from "@/types/chapter/ChapterPage";
-import {mapChapter} from "@/mappers/ChapterMapper";
-import {ChapterDTO} from "@/types/chapter/ChapterDTO";
 import {Chapter} from "@/models/Chapter";
+import {mapChapterPage} from "@/mappers/ChapterPageMapper";
+import {mapChapter} from "@/mappers/ChapterMapper";
+import type {ChapterPage} from "@/types/chapter/ChapterPage";
+import type {ChapterPageDTO} from "@/types/chapter/ChapterPageDTO";
+import type {ChapterDTO} from "@/types/chapter/ChapterDTO";
+import type {SeriesPageDTO} from "@/types/SeriesPageDTO";
+import {mapSeriesPage} from "@/mappers/SeriesPageMapper";
+import {Series} from "@/types/Series";
+import type {Page} from "@/types/Pagination";
 
 export class CatalogService {
 
@@ -24,5 +28,13 @@ export class CatalogService {
         );
 
         return mapChapter(response.data);
+    }
+
+    static async getSeries(params: { [k: string]: string | number; }): Promise<Page<Series>> {
+        const response = await api.get<SeriesPageDTO>(
+            `/api/catalogue/public/series/`,
+            {params: params}
+        );
+        return mapSeriesPage(response.data);
     }
 }
