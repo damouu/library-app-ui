955217364f94422846a71062fe101a1e69338e3d
damouu <mouadsehbaoui@gmail.com>
2026-07-05 19:20:59 +0900

    Refactor: AuthFlowService.ts (#LAU-43)

--- src/services/AuthFlowService.ts
+++ src/services/AuthFlowService.ts
@@ -1,18 +1,8 @@
 import {AuthService} from "@/services/AuthService";
-import {RecordService} from "@/services/RecordService";
 
 export class AuthFlowService {
 
     static async login(email: string, password: string) {
-
-        const {user, token} = await AuthService.signIn(email, password);
-
-        const records = await RecordService.getRecords(0, 5, token);
-
-        return {
-            user,
-            token,
-            records
-        };
+        return AuthService.signIn(email, password);
     }
 }
