8145f78354e417b323a511593b0c9798a98700e5
damouu <mouadsehbaoui@gmail.com>
2026-07-05 19:20:50 +0900

    Refactor: AnalyticService.ts (#LAU-43)

--- src/services/AnalyticService.ts
+++ src/services/AnalyticService.ts
@@ -1,23 +1,24 @@
 import {api} from "@/plugins/gateway";
 import {mapTopBorrowedChapter} from "@/mappers/TopBorrowedChapterMapper";
 import type {TopBorrowedChapter} from "@/types/chapter/TopBorrowedChapter";
-import type {TopBorrowedChapterDTO} from "@/types/chapter/TopBorrowedChapterDTO";
+import type {TopBorrowedChapterDTO} from "@/types/analytics/TopBorrowedChapterDTO";
 import type {PeriodKey} from "@/types/analytics/PeriodKey";
+import type {Page} from "@/types/Pagination";
 
 
 export class AnalyticService {
 
-    static async getTop(period: PeriodKey, page: number, size: number,): Promise<TopBorrowedChapter[]> {
+    static async getTop(period: PeriodKey, page: number, size: number,): Promise<Page<TopBorrowedChapter>> {
 
-        const response = await api.get<TopBorrowedChapterDTO[]>(
+        const response = await api.get<Page<TopBorrowedChapterDTO>>(
             "/api/analytics/public/top-chapters",
             {
-                params: {
-                    period, page, size
-                }
+                params: {period, page, size}
             }
         );
-
-        return response.data.map(mapTopBorrowedChapter);
+        return {
+            ...response.data,
+            content: response.data.content.map(mapTopBorrowedChapter)
+        };
     }
 }
