876de8a0170d62282e87643a0ea1616438ab5549
damouu <mouadsehbaoui@gmail.com>
2026-07-05 19:23:49 +0900

    Refactor: UserView.vue (#LAU-43)

--- src/views/user/UserView.vue
+++ src/views/user/UserView.vue
@@ -10,6 +10,7 @@
           <h4 class="mb-4">最近の貸出履歴</h4>
         </div>
         <BorrowHistory/>
+        <Pagination :total-pages="totalPages" :current-page="currentPage" :is-first="isFirst" :is-last="isLast"/>
       </div>
 
     </div>
@@ -19,4 +20,15 @@
 <script setup lang="ts">
 import BorrowHistory from '@/components/user/BorrowHistory.vue';
 import UserProfile from '@/components/user/UserProfile.vue';
+import Pagination from "@/components/common/Pagination.vue";
+import {useUserStore} from "@/stores/User";
+import {computed} from "vue";
+
+const userStore = useUserStore();
+
+const currentPage = computed(() => userStore.borrowHistory?.number ?? 0);
+const totalPages = computed(() => userStore.borrowHistory?.totalPages ?? 0);
+const isFirst = computed(() => userStore.borrowHistory?.first ?? true);
+const isLast = computed(() => userStore.borrowHistory?.last ?? true);
+
 </script>
