484a8505c4400f74e85972820ad1e9f3345b6d0c
damouu <mouadsehbaoui@gmail.com>
2026-07-10 11:32:20 +0900

    refactor: Inventory.ts

--- src/stores/Inventory.ts
+++ src/stores/Inventory.ts
@@ -1,10 +1,12 @@
-import {defineStore} from "pinia";
-import {Inventory} from "@/types/Inventory"
 import {ref} from 'vue';
+import {defineStore} from "pinia";
+import {Inventory} from "@/models/Inventory"
 import {InventoryService} from "@/services/InventoryService";
 
 export const useInventoryStore = defineStore('Inventory', () => {
+
     const currentChapter = ref<Inventory | null>(null);
+
     const isLoading = ref(false);
 
     async function checkAvailability(chapterUuid: string): Promise<void> {
@@ -16,6 +18,7 @@ export const useInventoryStore = defineStore('Inventory', () => {
             currentChapter.value = await InventoryService.checkAvailability(chapterUuid);
 
         } finally {
+
             isLoading.value = false;
         }
     }
