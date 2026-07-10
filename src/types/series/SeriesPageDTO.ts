41543544b718a60c932b39c92e861bc9f91781fd
damouu <mouadsehbaoui@gmail.com>
2026-07-10 11:35:10 +0900

    Refactor: SeriesPageDTO.ts

--- src/types/series/SeriesPageDTO.ts
+++ src/types/series/SeriesPageDTO.ts
@@ -1,6 +1,6 @@
-import type {PageableDTO} from "./PageableDTO";
+import type {PageableDTO} from "../common/PageableDTO";
 import type {SeriesDTO} from "@/types/chapter/SeriesDTO";
-import type {SortDTO} from "./SortDTO";
+import type {SortDTO} from "../common/SortDTO";
 
 export interface SeriesPageDTO {
     content: SeriesDTO[];
