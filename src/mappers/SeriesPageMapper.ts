d9aea41c78e79fe69d75a7e6da7502b1a3ca56c2
damouu <mouadsehbaoui@gmail.com>
2026-07-10 11:35:20 +0900

    Refactor: SeriesPageMapper.ts

--- src/mappers/SeriesPageMapper.ts
+++ src/mappers/SeriesPageMapper.ts
@@ -1,10 +1,20 @@
-import type {SeriesPageDTO} from "@/types/SeriesPageDTO";
-import type {SeriesPage} from "@/types/SeriesPage";
-import {mapSeries} from "./SeriesMapper";
+import type {ChapterPageDTO} from "@/types/chapter/ChapterPageDTO";
+import {Chapter} from "@/models/Chapter";
+import type {Page} from "@/types/common/Pagination";
+import {mapChapter} from "@/mappers/ChapterMapper";
 
-export function mapSeriesPage(dto: SeriesPageDTO): SeriesPage {
+export function mapChapterPage(dto: ChapterPageDTO): Page<Chapter> {
     return {
-        ...dto,
-        content: dto.content.map(mapSeries),
+        content: dto.content.map(mapChapter),
+        pageable: dto.pageable,
+        sort: dto.sort,
+        totalPages: dto.totalPages,
+        totalElements: dto.totalElements,
+        size: dto.size,
+        number: dto.number,
+        first: dto.first,
+        last: dto.last,
+        numberOfElements: dto.numberOfElements,
+        empty: dto.empty
     };
 }
