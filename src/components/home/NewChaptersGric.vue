16d5e8c14d9a23f2dd720e66d91d393c8d38c796
damouu <mouadsehbaoui@gmail.com>
2026-07-19 13:53:55 +0900

    Refactor: NewChaptersGric.vue

--- src/components/home/NewChaptersGric.vue
+++ src/components/home/NewChaptersGric.vue
@@ -6,28 +6,25 @@
     </div>
 
     <div v-if="chapterStore.newsLoading" class="row gy-5">
-      
+
       <div v-for="i in 12" :key="i" class="col-6 col-sm-4 col-md-3 col-lg-2">
 
         <div class="card h-100 shadow-sm border-2 placeholder-glow">
 
-          <div class="img-container">
-            <div class="placeholder w-100 custom-card-img"></div>
+          <div class="image-container overflow-hidden rounded-top">
+            <div class="placeholder w-100 ranking-img"></div>
           </div>
 
           <div class="card-body p-2 text-center">
-
             <span class="placeholder col-10 d-block mx-auto mb-2"></span>
-
             <span class="placeholder col-5 d-block mx-auto mb-2"></span>
-
             <span class="placeholder col-7 d-block mx-auto"></span>
-
           </div>
 
         </div>
       </div>
     </div>
+
 
     <TransitionGroup v-else name="fade-classic" tag="div" class="row gy-5">
 
@@ -38,20 +35,23 @@
             class="text-decoration-none text-reset text-center"
         >
           <div class="card h-100 shadow-sm border-2">
-            <div class="img-container">
-              <img :src="chapter.coverArtworkUrl" class="card-img-top custom-card-img" alt="Cover">
+
+            <div class="image-container overflow-hidden rounded-top">
+              <img :src="chapter.coverArtworkUrl" class="card-img-top ranking-img" alt="Cover">
             </div>
-            <div class="card-body p-2 text-center">
-              <h6 class="card-title text-primary mb-1 text-truncate text-center">
+
+            <div class="card-body p-2 d-flex flex-column align-items-center justify-content-center text-center">
+              <h6 class="card-title text-primary mb-1 text-truncate w-100">
                 {{ chapter.title }}
               </h6>
-              <p class="card-text mb-0 text-body-secondary text-muted">
+              <p class="card-text mb-0 text-body-secondary text-muted w-100">
                 巻 {{ chapter.chapterNumber }}
               </p>
-              <p class="card-text mb-0 text-body-secondary text-muted">
+              <p class="card-text mb-0 text-body-secondary text-muted w-100">
                 出版 {{ chapter.publicationDate }}
               </p>
             </div>
+
           </div>
         </router-link>
       </div>
@@ -74,8 +74,5 @@ const scrollToTop = () => {
   }
 };
 
-
 defineExpose({scrollToTop});
-
-
 </script>
