ede23fbb98526dda39ec0794c6978c644cbbb34d
damouu <mouadsehbaoui@gmail.com>
2026-09-22 23:16:51 +0900

    Refactor: Search.ts

--- src/stores/Search.ts
+++ src/stores/Search.ts
@@ -1,17 +1,28 @@
-import {defineStore} from 'pinia';
-import {ref} from 'vue';
-import {SearchService} from '@/services/SearchService';
-import type {Chapter} from '@/models/Chapter';
+import {defineStore} from "pinia";
+import {ref, shallowRef} from "vue";
 
-export const useSearchStore = defineStore('Search', () => {
-    const results = ref<Chapter[]>([]);
+import type {SearchPagination} from "@/services/SearchService";
+import {SearchService} from "@/services/SearchService";
+import type {Chapter} from "@/models/Chapter";
+
+export const useSearchStore = defineStore("Search", () => {
+    const results = shallowRef<Chapter[]>([]);
+    const pagination = ref<SearchPagination | null>(null);
+
     const isLoading = ref(false);
 
-    async function search(query: string): Promise<void> {
+    async function search(query: string, page: number): Promise<void> {
         isLoading.value = true;
 
         try {
-            results.value = await SearchService.search(query);
+            const response = await SearchService.search(
+                query,
+                page,
+                6
+            );
+
+            results.value = response.results;
+            pagination.value = response.pagination;
         } finally {
             isLoading.value = false;
         }
@@ -19,7 +30,14 @@ export const useSearchStore = defineStore('Search', () => {
 
     function clearResults(): void {
         results.value = [];
+        pagination.value = null;
     }
 
-    return {results, isLoading, search, clearResults,};
+    return {
+        results,
+        pagination,
+        isLoading,
+        search,
+        clearResults,
+    };
 });
