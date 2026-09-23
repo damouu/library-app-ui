4ada784fbdcf4b06fe8e5956f271d41fab9a473e
damouu <mouadsehbaoui@gmail.com>
2026-09-23 16:41:41 +0900

    Create: SearchChapterResponse.ts

---
+++ src/types/search/SearchChapterResponse.ts
@@ -0,0 +1,11 @@
+// // interface SearchChapterResponse {
+//     chapter_uuid: string;
+//     series_uuid: string;
+//     title: string;
+//     second_title: string;
+//     summary: string | null;
+//     chapter_number: number | null;
+//     total_pages: number | null;
+//     publication_date: string | null;
+//     cover_artwork_url: string;
+// }
